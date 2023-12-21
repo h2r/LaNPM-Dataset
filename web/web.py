114a727f5274c74a9e2807021dc5774e0ce6fedb
ahmedjaafar <ahmedjaafar@hotcake.rlab.cs.brown.edu>
2023-12-21 18:17:40 -0500

    added more web functionality and pages. still in progress.

--- web/web.py
+++ web/web.py
@@ -13,15 +13,20 @@ chrome_options.add_experimental_option("detach", True)
 
 driver = webdriver.Chrome(options=chrome_options)
 
-# Navigate to the AI2-THOR demo website
-# driver.get('https://ai2thor.allenai.org/demo/')
+# driver.get('file:///home/ahmedjaafar/web/scene1.html')
+# driver.implicitly_wait(1)
 
+driver.get('file:///home/ahmedjaafar/NPM-Dataset/web/index.html')
 
-driver.get('file:///home/ahmedjaafar/web/scene1.html')
-driver.implicitly_wait(1)
+# Wait for the user to manually click the 'Start' button and navigate to scene1.html
+wait = WebDriverWait(driver, 60)
+# Wait for the 'blocking-overlay' to be visible on scene1.html
+blocking_overlay = wait.until(EC.visibility_of_element_located((By.ID, 'blocking-overlay')))
 
 # Wait for the iframe to load
-wait = WebDriverWait(driver, 10)
+# wait = WebDriverWait(driver, 10)
+# wait.until(EC.frame_to_be_available_and_switch_to_it((By.ID, 'myIframe')))
+
 wait.until(EC.frame_to_be_available_and_switch_to_it((By.ID, 'myIframe')))
 
 time.sleep(12.25)
@@ -33,6 +38,73 @@ ithor_box.click()
 map = driver.find_elements(By.CLASS_NAME, "ant-card-cover")
 driver.execute_script("arguments[0].click();", map[5])
 
+
+# Now that scene2.html is loaded, you can interact with its elements
+# Example: Wait for the 'blocking-overlay' to be visible on scene2.html
+blocking_overlay = wait.until(EC.visibility_of_element_located((By.ID, 'blocking-overlay')))
+
+wait.until(EC.frame_to_be_available_and_switch_to_it((By.ID, 'myIframe')))
+
+time.sleep(12.25)
+
+ithor_box = driver.find_element(By.CLASS_NAME, "ant-checkbox-input")
+ithor_box.click()
+
+# time.sleep(1)
+map = driver.find_elements(By.CLASS_NAME, "ant-card-cover")
+driver.execute_script("arguments[0].click();", map[7])
+
+
+
+# Now that scene3.html is loaded, you can interact with its elements
+# Example: Wait for the 'blocking-overlay' to be visible on scene3.html
+blocking_overlay = wait.until(EC.visibility_of_element_located((By.ID, 'blocking-overlay')))
+
+wait.until(EC.frame_to_be_available_and_switch_to_it((By.ID, 'myIframe')))
+
+time.sleep(12.25)
+
+ithor_box = driver.find_element(By.CLASS_NAME, "ant-checkbox-input")
+ithor_box.click()
+
+# time.sleep(1)
+map = driver.find_elements(By.CLASS_NAME, "ant-card-cover")
+driver.execute_script("arguments[0].click();", map[10])
+
+
+
+
+# Now that scene4.html is loaded, you can interact with its elements
+# Example: Wait for the 'blocking-overlay' to be visible on scene4.html
+blocking_overlay = wait.until(EC.visibility_of_element_located((By.ID, 'blocking-overlay')))
+
+wait.until(EC.frame_to_be_available_and_switch_to_it((By.ID, 'myIframe')))
+
+time.sleep(12.25)
+
+ithor_box = driver.find_element(By.CLASS_NAME, "ant-checkbox-input")
+ithor_box.click()
+
+# time.sleep(1)
+map = driver.find_elements(By.CLASS_NAME, "ant-card-cover")
+driver.execute_script("arguments[0].click();", map[12])
+
+
+
+# Now that scene5.html is loaded, you can interact with its elements
+# Example: Wait for the 'blocking-overlay' to be visible on scene5.html
+blocking_overlay = wait.until(EC.visibility_of_element_located((By.ID, 'blocking-overlay')))
+
+wait.until(EC.frame_to_be_available_and_switch_to_it((By.ID, 'myIframe')))
+
+time.sleep(12.25)
+
+ithor_box = driver.find_element(By.CLASS_NAME, "ant-checkbox-input")
+ithor_box.click()
+
+# time.sleep(1)
+map = driver.find_elements(By.CLASS_NAME, "ant-card-cover")
+driver.execute_script("arguments[0].click();", map[16])
 
 breakpoint()
 
