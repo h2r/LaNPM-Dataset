18bd0a41e79cfe63a1bdafc837b351970e25ecd4
ahmedjaafar <ahmedjaafar@hotcake.rlab.cs.brown.edu>
2023-12-22 18:54:51 -0500

    works from start to finish through all the scenes and all the data is saved into the csv. still working on other parts.

--- web/web.py
+++ web/web.py
@@ -34,102 +34,106 @@ driver.get('file:///home/ahmedjaafar/NPM-Dataset/web/index.html')
 
 # Wait for the user to manually click the 'Start' button and navigate to scene1.html
 wait = WebDriverWait(driver, 60)
-# Wait for the 'blocking-overlay' to be visible on scene1.html
-blocking_overlay = wait.until(EC.visibility_of_element_located((By.ID, 'blocking-overlay')))
 
-# Wait for the iframe to load
-# wait = WebDriverWait(driver, 10)
-# wait.until(EC.frame_to_be_available_and_switch_to_it((By.ID, 'myIframe')))
+scenes = [5,7,10,12,15]
+scenes_ordered = [1,2,3,4,5]
+for i, j in zip(scenes, scenes_ordered):
+    # Wait for the 'blocking-overlay' to be visible on scene{j}.html
+    blocking_overlay = wait.until(EC.visibility_of_element_located((By.ID, 'blocking-overlay')))
 
-wait.until(EC.frame_to_be_available_and_switch_to_it((By.ID, 'myIframe')))
+    wait.until(EC.frame_to_be_available_and_switch_to_it((By.ID, 'myIframe')))
 
-time.sleep(13)
+    time.sleep(13)
 
-ithor_box = driver.find_element(By.CLASS_NAME, "ant-checkbox-input")
-ithor_box.click()
+    ithor_box = driver.find_element(By.CLASS_NAME, "ant-checkbox-input")
+    ithor_box.click()
 
-map = driver.find_elements(By.CLASS_NAME, "ant-card-cover")
-driver.execute_script("arguments[0].click();", map[5])
+    map = driver.find_elements(By.CLASS_NAME, "ant-card-cover")
+    driver.execute_script("arguments[0].click();", map[i])
 
 
-driver.switch_to.default_content()
-next_button = WebDriverWait(driver, 60).until(EC.element_to_be_clickable((By.ID, 'next-button')))
+    driver.switch_to.default_content()
 
-# Wait for the button to be clicked by the user
-WebDriverWait(driver, 60).until(lambda d: next_button.is_enabled())
-scene_data = extract_textbox_values(driver, f'Scene')
-all_scene_data.append(scene_data)
+
+    next_button = WebDriverWait(driver, 300).until(EC.element_to_be_clickable((By.ID, 'next-button')))
+    WebDriverWait(driver, 300).until(EC.visibility_of_element_located((By.ID, 'submission-message')))
+
+
+    scene_data = extract_textbox_values(driver, f'Scene{j}')
+    all_scene_data.append(scene_data)
+
+
 df = pd.DataFrame(all_scene_data)
 df.to_csv('scene_commands.csv', index=False)
 
 
 
-# Now that scene2.html is loaded, you can interact with its elements
-# Example: Wait for the 'blocking-overlay' to be visible on scene2.html
-blocking_overlay = wait.until(EC.visibility_of_element_located((By.ID, 'blocking-overlay')))
+# # Now that scene2.html is loaded, you can interact with its elements
+# # Example: Wait for the 'blocking-overlay' to be visible on scene2.html
+# blocking_overlay = wait.until(EC.visibility_of_element_located((By.ID, 'blocking-overlay')))
 
-wait.until(EC.frame_to_be_available_and_switch_to_it((By.ID, 'myIframe')))
+# wait.until(EC.frame_to_be_available_and_switch_to_it((By.ID, 'myIframe')))
 
-time.sleep(12.75)
+# time.sleep(12.75)
 
-ithor_box = driver.find_element(By.CLASS_NAME, "ant-checkbox-input")
-ithor_box.click()
+# ithor_box = driver.find_element(By.CLASS_NAME, "ant-checkbox-input")
+# ithor_box.click()
 
-# time.sleep(1)
-map = driver.find_elements(By.CLASS_NAME, "ant-card-cover")
-driver.execute_script("arguments[0].click();", map[7])
-
-
-
-# Now that scene3.html is loaded, you can interact with its elements
-# Example: Wait for the 'blocking-overlay' to be visible on scene3.html
-blocking_overlay = wait.until(EC.visibility_of_element_located((By.ID, 'blocking-overlay')))
-
-wait.until(EC.frame_to_be_available_and_switch_to_it((By.ID, 'myIframe')))
-
-time.sleep(12.25)
-
-ithor_box = driver.find_element(By.CLASS_NAME, "ant-checkbox-input")
-ithor_box.click()
-
-# time.sleep(1)
-map = driver.find_elements(By.CLASS_NAME, "ant-card-cover")
-driver.execute_script("arguments[0].click();", map[10])
+# # time.sleep(1)
+# map = driver.find_elements(By.CLASS_NAME, "ant-card-cover")
+# driver.execute_script("arguments[0].click();", map[7])
 
 
 
+# # Now that scene3.html is loaded, you can interact with its elements
+# # Example: Wait for the 'blocking-overlay' to be visible on scene3.html
+# blocking_overlay = wait.until(EC.visibility_of_element_located((By.ID, 'blocking-overlay')))
 
-# Now that scene4.html is loaded, you can interact with its elements
-# Example: Wait for the 'blocking-overlay' to be visible on scene4.html
-blocking_overlay = wait.until(EC.visibility_of_element_located((By.ID, 'blocking-overlay')))
+# wait.until(EC.frame_to_be_available_and_switch_to_it((By.ID, 'myIframe')))
 
-wait.until(EC.frame_to_be_available_and_switch_to_it((By.ID, 'myIframe')))
+# time.sleep(12.25)
 
-time.sleep(12.25)
+# ithor_box = driver.find_element(By.CLASS_NAME, "ant-checkbox-input")
+# ithor_box.click()
 
-ithor_box = driver.find_element(By.CLASS_NAME, "ant-checkbox-input")
-ithor_box.click()
-
-# time.sleep(1)
-map = driver.find_elements(By.CLASS_NAME, "ant-card-cover")
-driver.execute_script("arguments[0].click();", map[12])
-
+# # time.sleep(1)
+# map = driver.find_elements(By.CLASS_NAME, "ant-card-cover")
+# driver.execute_script("arguments[0].click();", map[10])
 
 
-# Now that scene5.html is loaded, you can interact with its elements
-# Example: Wait for the 'blocking-overlay' to be visible on scene5.html
-blocking_overlay = wait.until(EC.visibility_of_element_located((By.ID, 'blocking-overlay')))
 
-wait.until(EC.frame_to_be_available_and_switch_to_it((By.ID, 'myIframe')))
 
-time.sleep(12.25)
+# # Now that scene4.html is loaded, you can interact with its elements
+# # Example: Wait for the 'blocking-overlay' to be visible on scene4.html
+# blocking_overlay = wait.until(EC.visibility_of_element_located((By.ID, 'blocking-overlay')))
 
-ithor_box = driver.find_element(By.CLASS_NAME, "ant-checkbox-input")
-ithor_box.click()
+# wait.until(EC.frame_to_be_available_and_switch_to_it((By.ID, 'myIframe')))
 
-# time.sleep(1)
-map = driver.find_elements(By.CLASS_NAME, "ant-card-cover")
-driver.execute_script("arguments[0].click();", map[16])
+# time.sleep(12.25)
+
+# ithor_box = driver.find_element(By.CLASS_NAME, "ant-checkbox-input")
+# ithor_box.click()
+
+# # time.sleep(1)
+# map = driver.find_elements(By.CLASS_NAME, "ant-card-cover")
+# driver.execute_script("arguments[0].click();", map[12])
+
+
+
+# # Now that scene5.html is loaded, you can interact with its elements
+# # Example: Wait for the 'blocking-overlay' to be visible on scene5.html
+# blocking_overlay = wait.until(EC.visibility_of_element_located((By.ID, 'blocking-overlay')))
+
+# wait.until(EC.frame_to_be_available_and_switch_to_it((By.ID, 'myIframe')))
+
+# time.sleep(12.25)
+
+# ithor_box = driver.find_element(By.CLASS_NAME, "ant-checkbox-input")
+# ithor_box.click()
+
+# # time.sleep(1)
+# map = driver.find_elements(By.CLASS_NAME, "ant-card-cover")
+# driver.execute_script("arguments[0].click();", map[16])
 
 breakpoint()
 
