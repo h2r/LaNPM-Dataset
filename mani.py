89f499e5d75a292977eb8b62532fa17e97266711
ahmedjaafar <ahmedjaafar@hotcake.rlab.cs.brown.edu>
2024-03-12 19:56:18 -0400

    reorganized the way data is saved and made visuals save as npy files instead of converting them to lists

--- mani.py
+++ mani.py
@@ -5,25 +5,25 @@ import argparse
 parser = argparse.ArgumentParser(description="Run the controller with specified scene")
 
 # Step 3: Add the scene argument
-parser.add_argument("--scene", type=str, required=True, help="Specify the scene to load. Either '13', '51', '75', '81', '123'")
+parser.add_argument("--scene", type=str, required=True, help="Specify the scene to load. Either 's1', 's2', 's3', 's4', 's5'")
 parser.add_argument("--command", type=str, required=True, help="Specify the natural language command")
 
 # Step 4: Parse the command line arguments
 args = parser.parse_args()
 
 scene = ''
-if args.scene == '13':
+if args.scene == 's1':
     scene = 'FloorPlan_Train1_3'
-elif args.scene == '51':
+elif args.scene == 's2':
     scene = 'FloorPlan_Train5_1'
-elif args.scene == '75':
+elif args.scene == 's3':
     scene = 'FloorPlan_Train7_5'
-elif args.scene == '81':
+elif args.scene == 's4':
     scene = 'FloorPlan_Train8_1'
-elif args.scene == '123':
+elif args.scene == 's5':
     scene = 'FloorPlan_Train12_3'
 else:
-    raise ValueError(f"Invalid scene name: {args.scene}. Pick from '13', '51', '75', '81', '123'")
+    raise ValueError(f"Invalid scene name: {args.scene}. Pick from 's1', 's2', 's3', 's4', 's5'")
 
 if __name__ == "__main__":
     controller = Controller(
