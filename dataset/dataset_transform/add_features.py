cae368cac5bbf7ff2a97a5d29784a5f0d72fa005
Ahmed Jaafar <ajaafar@umass.edu>
2024-11-14 18:53:44 -0500

    modified the add_features.py to add more features to edit the dataset.

--- dataset/dataset_transform/add_features.py
+++ dataset/dataset_transform/add_features.py
@@ -3,6 +3,7 @@ import h5py
 from ai2thor.controller import Controller
 import os
 import openai
+from tqdm import tqdm
 import numpy as np
 
 def get_event(scene):
@@ -64,6 +65,10 @@ scene_objs_json = None
 
 PATH = "/users/ajaafar/data/shared/lanmp/sim_dataset.hdf5" #HDF5 dataset path
 NEW_PATH = "/users/ajaafar/data/shared/lanmp/sim_dataset_augmented.hdf5" #augmented HDF5 dataset path
+no_pickup_release = []
+commands_scenes={}
+cmd_id = {}
+cmd_scene = {}
 
 # with h5py.File(PATH, 'r') as hdf_file:
 with h5py.File(PATH, 'r') as hdf_file, h5py.File(NEW_PATH, 'w') as new_hdf_file:
@@ -78,17 +83,28 @@ with h5py.File(PATH, 'r') as hdf_file, h5py.File(NEW_PATH, 'w') as new_hdf_file:
         traj_goal_pos = json.load(json_file)   
     with open("obj_pos_dic.json", "r") as json_file:
         obj_pos_dic = json.load(json_file)   
+    with open("no_pickup_release.json", "r") as json_file:
+        no_pickup_release = json.load(json_file) 
+         
+    
+    # print(len(no_pickup_release))
+    # exit()
 
-    for trajectory_name, trajectory_group in hdf_file.items():
+    for trajectory_name, trajectory_group in tqdm(hdf_file.items(), desc="Processing trajectories"):
+        # print(len(hdf_file.items()))
+        # print(len(cmd_scene.items()))
+        # breakpoint()
         # print(f"Trajectory: {trajectory_name}")
-        if trajectory_name == "data_20:07:12":
-            continue
+        # if trajectory_name not in no_pickup_release:
+        #     continue
         i = 0
         # obj_pos = None
         new_trajectory_group = new_hdf_file.create_group(trajectory_name)
-
+        
+        actions = [] #
         # dist = np.inf
         # Iterate through each timestep group within the trajectory
+        flag = False
         for timestep_name, timestep_group in trajectory_group.items():
             # print(f"  Step: {timestep_name}")
             new_timestep_group = new_trajectory_group.create_group(timestep_name)
@@ -115,36 +131,67 @@ with h5py.File(PATH, 'r') as hdf_file, h5py.File(NEW_PATH, 'w') as new_hdf_file:
             #         with open("scene_objs.json", "r") as json_file:
             #             scene_objs_json = json.load(json_file)
             
-            # obj = traj_obj_dic[trajectory_name] #load obj from saved dict
+            obj = traj_obj_dic[trajectory_name] #load obj from saved dict
             # event = get_event(scene)
             # obj_pos = get_obj_pos(event, obj)
             obj_pos = obj_pos_dic[trajectory_name]
             ee_pos = metadata['steps'][0]['state_ee'][:3]
             body_pos = metadata['steps'][0]['state_body'][:3]
             action = metadata['steps'][0]['action']
-            held_objs = metadata['steps'][0]['held_objs']
-            dist_to_object = np.linalg.norm(np.array(list(obj_pos.values())) - np.array(ee_pos))
-            dist_to_goal = np.linalg.norm(np.array(traj_goal_pos[trajectory_name][:3] - np.array(body_pos)))
+            # print(action)
+            # if action == "LookUp":
+            #     breakpoint()
+            # continue
+            
+            # actions.append(action) #
+            # continue #
+
+            # held_objs = metadata['steps'][0]['held_objs']
+            # dist_to_object = np.linalg.norm(np.array(list(obj_pos.values())) - np.array(ee_pos))
+            # dist_to_goal = np.linalg.norm(np.array(traj_goal_pos[trajectory_name][:3] - np.array(body_pos)))
             if i == 0:
-                # command = metadata['nl_command']
+                command = metadata['nl_command']
+                # print(command)
+                # commands_scenes[command] = scene
+                # cmd_id[command] = trajectory_name
+
+                if command in cmd_scene.keys():
+                    breakpoint()
+                else:
+                    cmd_scene[command] = scene
+                i+=1
+                # print(command)
                 # obj = extract_obj(command, scene_objs_json, scene)
                 # traj_obj_dic[trajectory_name] = obj
                 # obj = traj_obj_dic[trajectory_name] #load obj from saved dict
                 # event = get_event(scene)    
                 # obj_pos = get_obj_pos(event, obj)
-                obj_pos_dic[trajectory_name] = obj_pos
-                metadata['target_obj'] = traj_obj_dic[trajectory_name] 
-                metadata['init_target_obj_pos'] = obj_pos
-                metadata['goal_pos'] = traj_goal_pos[trajectory_name][:3]
-                metadata["base_start_to_goal_dist"] = dist_to_goal
-                metadata['ee_start_to_target_obj_dist'] = dist_to_object
-            i+=1
+                # obj_pos_dic[trajectory_name] = obj_pos
+                # metadata['target_obj'] = traj_obj_dic[trajectory_name] 
+                # metadata['init_target_obj_pos'] = obj_pos
+                # metadata['goal_pos'] = traj_goal_pos[trajectory_name][:3]
+                # metadata["base_start_to_goal_dist"] = dist_to_goal
+                # metadata['ee_start_to_target_obj_dist'] = dist_to_object
+            # i+=1
+            continue
             
-            
-            if action == "PickupObject" and not held_objs:
+            # if action == "PickupObject":
+            #     print(held_objs)
+            #     breakpoint()
+            # continue
+            if action == "PickupObject" and held_objs:
+                flag = True
+            if flag:
                 metadata['steps'][0]['curr_ee_to_target_obj_dist'] = float(0)
             else:
                 metadata['steps'][0]['curr_ee_to_target_obj_dist'] = dist_to_object
+            
+            print(action)
+            continue
+            print(held_objs)
+            print(metadata['steps'][0]['curr_ee_to_target_obj_dist'])
+            # print()
+            continue
 
             metadata['steps'][0]['curr_base_to_goal_dist'] = dist_to_goal
             
@@ -160,6 +207,30 @@ with h5py.File(PATH, 'r') as hdf_file, h5py.File(NEW_PATH, 'w') as new_hdf_file:
                 if attr_name != 'metadata':
                     new_timestep_group.attrs[attr_name] = attr_value
             # NEW CODE END
+        
+        continue
+        if "PickupObject" not in actions:
+            no_pickup_release.append(trajectory_name)
+        if "ReleaseObject" not in actions:
+            no_pickup_release.append(trajectory_name)
+
+        first_value = 'PickupObject'
+        second_value = 'ReleaseObject'
+
+        # Find the first occurrence of the first value
+        if first_value in actions:
+            first_value_index = actions.index(first_value)
+            
+            # Check if there's any occurrence of the second value after the first
+            if second_value in actions[first_value_index + 1:]:
+                # print(f"{second_value} comes after {first_value} in the list.")
+                x=0
+            else:
+                no_pickup_release.append(trajectory_name)
+        else:
+            # print(f"{first_value} is not in the list.")
+            x=0
+        continue
 
         print(f'Finished {j} trajectories...')
         j+=1
@@ -169,4 +240,10 @@ with h5py.File(PATH, 'r') as hdf_file, h5py.File(NEW_PATH, 'w') as new_hdf_file:
     #     json.dump(traj_obj_dic, json_file, indent=4)
     # with open("obj_pos_dic.json", "w") as json_file:
     #     json.dump(obj_pos_dic, json_file, indent=4)
+    # with open("commands_scenes.json", "w") as f:
+    #     json.dump(commands_scenes, f)
+    # with open("cmd_id.json", "w") as f:
+    #     json.dump(cmd_id, f)
+    with open("cmd_scene.json", "w") as f:
+        json.dump(cmd_scene, f)
     print("FINISHED ALL!")
